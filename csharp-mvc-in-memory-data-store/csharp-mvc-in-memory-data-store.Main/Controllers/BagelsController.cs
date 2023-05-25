3dcaee391618a54080fcd8f14f84df7ceb23174d
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-05-25 19:11:42 +0300

    comepleted added methods to Delete and update Bagel in BagelRepo Refactor the put method and added delete/find a bagel/create

--- csharp-mvc-in-memory-data-store/csharp-mvc-in-memory-data-store.Main/Controllers/BagelsController.cs
+++ csharp-mvc-in-memory-data-store/csharp-mvc-in-memory-data-store.Main/Controllers/BagelsController.cs
@@ -28,19 +28,70 @@ namespace mvc_in_memory_data_store.Controllers
                 return Results.Problem(ex.Message);
             }
         }
-        [HttpPut]
-        public async Task<IResult> Put(Bagel bagel)
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IResult> GetBagel(int id)
         {
             try
             {
-                if (_bagelRepository.Add(bagel)) return Results.Ok();
-                return Results.NotFound();
+                var bagel = _bagelRepository.find(id);
+                return bagel.Id == id ? Results.Ok(bagel) : Results.NotFound($"Bagel withd id: {id} not found");
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IResult> Put(int id, Bagel bagel)
+        {
+            try
+            {
+                var result = _bagelRepository.UpdateBagel(id, bagel);
+
+                return result != null ? Results.Ok(result) : Results.NotFound($"Bagel withd id: {id} not found");
+               
 
             }
             catch (Exception ex)
             {
                 return Results.Problem(ex.Message);
             }
+        }
+
+        [HttpPost]
+        public async Task<IResult> CreateBagel(Bagel bagel)
+        {
+            try
+            {
+
+                if(bagel == null) { return  Results.Problem("bagel is empty"); }
+                 bagel.Id = _bagelRepository.findAll().Max(x=>x.Id) + 1;
+                _bagelRepository.Add(bagel);
+                // _bagelRepository.create(bagel.Type, bagel.Price);
+                return Results.Created("ok", bagel);
+            }catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+
+        }
+
+        [HttpDelete]
+        public async Task<IResult> DeleteBagel(int id)
+        {
+            try
+            {
+                var bagel = _bagelRepository.DeleteBagel(id);
+
+                return bagel!=null ? Results.Ok(bagel) : Results.NotFound($"Bagel withd id: {id} not found");
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+
         }
     }
 }
--- csharp-mvc-in-memory-data-store/csharp-mvc-in-memory-data-store.Main/Data/BagelRepository.cs
+++ csharp-mvc-in-memory-data-store/csharp-mvc-in-memory-data-store.Main/Data/BagelRepository.cs
@@ -8,10 +8,24 @@ namespace mvc_in_memory_data_store.Models
         private static int IdCounter = 1;
         private static List<Bagel> _bagels = new List<Bagel>();
 
-        public void create(String type, int price)
+        public int counterID { get { return IdCounter++; } }
+        public BagelRepository()
         {
-            Bagel bagel = new Bagel(BagelRepository.IdCounter++, type, price);
+            if(_bagels.Count == 0)
+            {
+            Bagel bagel = new Bagel(BagelRepository.IdCounter, "plain", 10);
+
+            _bagels.Add(bagel);
+
+            }
+        }
+
+        public Bagel create(string type, int price)
+        {
+            int id = _bagels.Count;
+            Bagel bagel = new Bagel(id++, type, price);
             BagelRepository._bagels.Add(bagel);
+            return bagel;
         }
 
         public List<Bagel> findAll()
@@ -22,7 +36,14 @@ namespace mvc_in_memory_data_store.Models
 
         public Bagel find(int id)
         {
-            return BagelRepository._bagels.First(bagel => bagel.getId() == id);
+            return BagelRepository._bagels.First(bagel => bagel.Id == id);
+        }
+
+        public IEnumerable<Bagel> DeleteBagel(int id)
+        {
+            var bagel = find(id);
+            _bagels.Remove(bagel);
+            return BagelRepository._bagels;
         }
 
         public bool Add(Bagel bagel)
@@ -34,5 +55,28 @@ namespace mvc_in_memory_data_store.Models
             }
             return false;
         }
+       public Bagel UpdateBagel(int id, Bagel b)
+        {
+            if (_bagels.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                foreach (Bagel bagel in _bagels)
+                {
+                    if (bagel.Id.Equals(id))
+                    {
+                        bagel.Price = b.Price;
+                        bagel.Type = b.Type;
+                        return bagel;
+                    }
+                }
+                return null;
+            }
+
+        }
+
+
     }
 }
