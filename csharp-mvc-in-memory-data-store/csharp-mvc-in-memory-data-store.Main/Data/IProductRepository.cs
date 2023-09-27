f6456a96aa1f117c00ae68568f77dda9c54210a4
spiroskafiris <spiroskafiris@gmail.com>
2023-09-27 19:57:01 +0300

    Spiros Kafiris

---
+++ csharp-mvc-in-memory-data-store/csharp-mvc-in-memory-data-store.Main/Data/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using mvc_in_memory_data_store.Models;
+
+namespace mvc_in_memory_data_store.Data
+{
+    public interface IProductRepository
+    {
+        void create(String name, string category, int price);       
+        List<Product> findAll();     
+        Product find(int id);
+        bool Add(Product product);
+        void Delete(int id);
+    }
+}
