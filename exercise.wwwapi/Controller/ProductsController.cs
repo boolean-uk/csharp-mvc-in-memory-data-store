412b24584184e8b7a1ca48611e14333bf1a7b611
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-25 14:30:38 +0100

    Created endpoints/controller and IRepository interface

---
+++ exercise.wwwapi/Controller/ProductsController.cs
@@ -0,0 +1,104 @@
+﻿using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exercise.wwwapi.Controller
+{
+    public static class ProductsController
+    {
+        public static void ConfigureProductsEndpoint(this WebApplication app) 
+        {
+            var productsGroup = app.MapGroup("products");
+
+            productsGroup.MapPost("/", PostProduct);
+            productsGroup.MapGet("/", GetProducts);
+            productsGroup.MapGet("/{id}", GetSpecificProduct);
+            productsGroup.MapPut("/{id}", PutProduct);
+            productsGroup.MapDelete("/{id}", DeleteProduct);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public static async Task<IResult> PostProduct(IRepository repository, string name, string category, int price)
+        {
+            if ((name == null) || (category == null)) 
+            {
+                return TypedResults.BadRequest("Name and/or category must be provided.");
+            }
+
+            Product? prod = repository.PostProduct(new ProductPost(name, category, price));
+            if (price < 0) // Int will never not be int. That will never happen. Checking for positive int instead
+            {
+                return TypedResults.BadRequest("Price must be an integer, larger than 0, something else was provided.");
+            }
+            if (prod == null) 
+            {
+                return TypedResults.BadRequest("Product with the provided name already exists.");
+            }
+            return TypedResults.Created($"/{prod.Id}", prod);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static async Task<IResult> GetProducts(IRepository repository, string? category) 
+        {
+            IEnumerable<Product?> prods = repository.GetProducts(category);
+            if (prods.Count() > 0)
+            {
+                return TypedResults.Ok(prods);
+            }
+            else 
+            {
+                return TypedResults.NotFound("No products in the provided category were found");
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static async Task<IResult> GetSpecificProduct(IRepository repository, int id) 
+        {
+            Product? prod = repository.GetSpecificProduct(id);
+            if (prod != null)
+            {
+                return TypedResults.Ok(prod);
+            }
+            else 
+            {
+                return TypedResults.NotFound("Product not found.");
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static async Task<IResult> PutProduct(IRepository repository, int id, string? name, string? category, int price) 
+        {
+            Tuple<Product?, int> res = repository.PutProduct(id, new ProductPut() { Name = name, Category = category, Price = price});
+            if (res.Item2 == 201)
+            {
+                return TypedResults.Created($"/{res.Item1.Id}", res.Item1);
+            } else if (res.Item2 == 400)
+            {
+                return TypedResults.BadRequest("Product with provided name already exists.");
+            } else 
+            {
+                return TypedResults.NotFound("Product not found.");
+            }                    
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static async Task<IResult> DeleteProduct(IRepository repository, int id) 
+        {
+            Product prod = repository.DeleteProduct(id);
+            if (prod is null)
+            {
+                return TypedResults.NotFound("Product not found.");
+            }
+            else 
+            {
+                return TypedResults.Ok(prod);
+            }
+        }
+    }
+}
---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IRepository
+    {
+        /// <summary>
+        /// Determine if the provided name is already located in the database.
+        /// </summary>
+        /// <param name="name"> string - the name of the <see cref="Product"/></param>
+        /// <returns> bool - true if the name is available, false otherwise </returns>
+        bool ProductNameIsAvailable(string? name);
+
+        /// <summary>
+        /// Post/Create a new <see cref="Product"/> and add to the database. 
+        /// </summary>
+        /// <param name="prod"> <see cref="ProductPost"/> - A prototype <see cref="Product"/> to be transcribed into a <see cref="Product"/> object</param>
+        /// <returns> <see cref="Product"/> - The created <see cref="Product"/>, null if an error occured</returns>
+        Product? PostProduct(ProductPost prod);
+
+        /// <summary>
+        /// Retrieve all <see cref="Product"/> within a specified category. Category can be empty, which would return all <see cref="Product"/> in the database.
+        /// </summary>
+        /// <param name="category"> string - Category to be returned. Can be null</param>
+        /// <returns>IEnumerable - All <see cref="Product"/> with a category matching the provided category.</returns>
+        IEnumerable<Product> GetProducts(string? category);
+
+        /// <summary>
+        /// Retrieve a specific <see cref="Product"/> from the database based on <see cref="Product"/>.Id
+        /// </summary>
+        /// <param name="id">int - the Id to be retrieved</param>
+        /// <returns> <see cref="Product"/> - the <see cref="Product"/> that had the provided Id, null if no match found.</returns>
+        Product? GetSpecificProduct(int id);
+
+        /// <summary>
+        /// Put/Update a <see cref="Product"/> in the database, based on provided Id.
+        /// </summary>
+        /// <param name="id"> int - The Id of the <see cref="Product"/> to be updated</param>
+        /// <param name="prod"> <see cref="ProductPut"/> - the temporary object to be transcribed into a <see cref="Product"/></param>
+        /// <returns> Tuple - item1 is a returned <see cref="Product"/>, item2 is error codes; either 201 (created), 400 (name already exists), or 404 (the product with provided id was not found).</returns>
+        Tuple<Product?, int> PutProduct(int id, ProductPut prod);
+
+        /// <summary>
+        /// Attempt to delete a <see cref="Product"/> based on the <see cref="Product"/> Id.
+        /// </summary>
+        /// <param name="id"> int - the id of the <see cref="Product"/> to be deleted from the database</param>
+        /// <returns><see cref="Product"/> - The deleted <see cref="Product"/> if found, null if <see cref="Product"/> was not found.</returns>
+        Product? DeleteProduct(int id);
+
+    }
+}
