24e00b657aa65ef31d9fb357bfb4535f60792d0d
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-01-25 13:26:18 +0100

    Henrik Rosenkilde c# ex3

---
+++ exercise.wwwapi/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        Product CreateProduct(Product product);
+        Product UpdateProduct(int id, ProductPut productPut);
+        void DeleteProduct(int id);
+    }
+}
---
+++ exercise.wwwapi/Repository/ProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace exercise.wwwapi.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDbContext _db;
+
+        public ProductRepository(ProductDbContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public IEnumerable<Product> GetProducts() 
+        {
+            return _db.Products.ToList();   
+        }
+
+        public Product GetProductById (int id) 
+        {
+            return _db.Products.Find(id);
+        }
+
+        public Product CreateProduct (Product product) 
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return product;
+        }
+
+        public Product UpdateProduct (int id, ProductPut productPut) 
+        {
+            var existingProduct = _db.Products.Find(id);
+            if (existingProduct == null) 
+            {
+                return null;
+            }
+
+            existingProduct.Price = productPut.Price;
+            existingProduct.Category = productPut.Category;
+            existingProduct.Name = productPut.Name;
+
+            _db.SaveChanges();
+            return existingProduct;
+        }
+
+        public void DeleteProduct (int id)
+        {
+            var product = _db.Products.Find(id);
+
+            if (product != null)
+            {
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _db.Products.ToList();
+        }
+    }
+}
