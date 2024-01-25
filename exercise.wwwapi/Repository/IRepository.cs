c59bfb7318852a2ac2efadb6384d33cbcc244e04
sebHanssen <sebastian.hanssen@outlook.com>
2024-01-25 12:36:52 +0100

    finish exercise

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> GetProducts();
+        IEnumerable<Product> GetProducts(string category);
+        Product AddProduct(Product product);
+        Product UpdateProduct(int id, ProductPut product);
+        Product GetAProduct(int id);
+        Product DeleteProduct(int id);
+    }
+}
