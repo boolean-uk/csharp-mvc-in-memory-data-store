812e7ef6788b3ffc99f9ca6a659cec90fd9f1b55
Mateusz Pietrzak <matti3301@gmail.com>
2024-01-25 14:55:31 +0100

    Completed all tasks

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product AddProduct(Product product);
+        Product GetProduct(int id);
+        Product UpdateProduct(int id, string newName, string newCategory, int newPrice);
+        Product DeleteProduct(int id);
+        IEnumerable<Product> GetProductsByCategory(string category);
+        Product GetProductByName(string name);
+    }
+}
