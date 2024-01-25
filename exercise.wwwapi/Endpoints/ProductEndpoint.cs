c815d5dc248843af8d91355e98314bac4addbdfb
kristianverduin <kristianverduin@gmail.com>
2024-01-25 13:06:29 +0100

    Finished core + extension

---
+++ exercise.wwwapi/Endpoints/ProductEndpoint.cs
@@ -0,0 +1,105 @@
+﻿using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exercise.wwwapi.Endpoints
+{
+    public static class ProductEndpoint
+    {
+        public static void ConfigureProductEndpoint(this WebApplication app)
+        {
+            var productGroup = app.MapGroup("products");
+
+            productGroup.MapPost("/", AddProduct);
+
+            productGroup.MapGet("/", GetProducts);
+
+            productGroup.MapGet("/{id}", GetAProduct);
+
+            productGroup.MapPut("/{id}", UpdateProduct);
+
+            productGroup.MapDelete("/{id}", DeleteProduct);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public static async Task<IResult> AddProduct(IRepository repository, ProductPost product)
+        {
+            if (!int.TryParse(product.Price, out var price))
+            {
+                return TypedResults.BadRequest("Price must be an integer, something else was provided");
+            }
+
+            var newProduct = new Product() { Name = product.Name, Category = product.Category.ToLower(), Price = price };
+            newProduct = repository.AddProduct(newProduct);
+
+            if (newProduct == null)
+            {
+                return TypedResults.BadRequest("Product with provided name already exists");
+            }
+
+            return TypedResults.Created("/", newProduct);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> GetProducts(IRepository repository, string category)
+        {
+            var products = repository.GetProducts(category.ToLower());
+
+            if (products.Count() == 0)
+            {
+                return TypedResults.NotFound("No products of the provided category were found");
+            }
+
+            return TypedResults.Ok(products);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> GetAProduct(IRepository repository, int id)
+        {
+            var product = repository.GetAProduct(id);
+
+            if (product == null)
+            {
+                return TypedResults.NotFound("Product not found");
+            }
+
+            return TypedResults.Ok(product);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public static async Task<IResult> UpdateProduct(IRepository repository, int id, ProductPost product)
+        {
+            if (!int.TryParse(product.Price, out _))
+            {
+                return TypedResults.BadRequest("Price must be an integer, something else was provided");
+            }
+
+            var newProduct = repository.UpdateProduct(id, product);
+
+            if (newProduct == null)
+            {
+                return TypedResults.NotFound("Product not found");
+            }
+
+            if (newProduct.Name == null)
+            {
+                return TypedResults.BadRequest("Product with provided name already exists");
+            }
+
+            return TypedResults.Created("/", newProduct);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> DeleteProduct(IRepository repository, int id)
+        {
+            var product = repository.DeleteProduct(id);
+
+            if (product == null)
+            {
+                return TypedResults.NotFound("Product not found");
+            }
+
+            return TypedResults.Ok(product);
+        }
+    }
+}
---
+++ exercise.wwwapi/Repository/Repository.cs
@@ -0,0 +1,74 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public class Repository : IRepository
+    {
+        private DataContext _db;
+
+        public Repository(DataContext db)
+        {
+            _db = db;
+        }
+
+        public Product? AddProduct(Product product)
+        {
+            if (_db.Products.FirstOrDefault(p => p.Name == product.Name) != null)
+            {
+                return null;
+            }
+
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return product;
+        }
+
+        public IEnumerable<Product> GetProducts(string category)
+        {
+            return _db.Products.Where(product => product.Category == category).ToList();
+        }
+
+        public Product? GetAProduct(int id)
+        {
+            return _db.Products.FirstOrDefault(product => product.Id == id);
+        }
+
+        public Product? UpdateProduct(int id, ProductPost product)
+        {
+
+            var newProduct = _db.Products.FirstOrDefault(p => p.Id == id);
+
+            if (newProduct == null)
+            {
+                return null;
+            }
+
+            if (_db.Products.FirstOrDefault(p => p.Name == product.Name) != null)
+            {
+                newProduct.Name = null;
+                return newProduct;
+            }
+
+            newProduct.Name = product.Name;
+            newProduct.Category = product.Category.ToLower();
+            newProduct.Price = int.Parse(product.Price);
+            _db.SaveChanges();
+            return newProduct;
+        }
+
+        public Product? DeleteProduct(int id)
+        {
+            var deletedProduct = _db.Products.FirstOrDefault(product => product.Id == id);
+
+            if (deletedProduct == null)
+            {
+                return null;
+            }
+
+            _db.Remove(deletedProduct);
+            _db.SaveChanges();
+            return deletedProduct;
+        }
+    }
+}
