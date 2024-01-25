530392c62ee70b5d5de18eb9ad12398994babebf
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-25 12:45:06 +0100

    core and first extension

---
+++ exercise.wwwapi/Models/ErrorTypes.cs
@@ -0,0 +1,19 @@
+﻿namespace exercise.wwwapi.Models
+{
+    public class ErrorTypes
+    {
+        public string message {  get; set; }
+        int id { get; set; }
+
+        public ErrorTypes(string Message, int id)
+        {
+            message = Message;
+            this.id = id;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+    }
+}
