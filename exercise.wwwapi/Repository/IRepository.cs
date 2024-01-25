753ed0817c4db08500344bb4398615576743d258
Philip Morud <philip.morud@no.experis.com>
2024-01-25 14:04:56 +0100

    Core and Extension

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IRepository
+    {
+        Product AddProduct(ProductBody product);
+        List<Product> GetProducts(string? category);
+        Product GetProduct(int id);
+        Product UpdateProduct(int id, ProductBody product);
+        void DeleteProduct(int id);
+        bool NameExists(string name);
+        bool IdExists(int id);
+    }
+}
