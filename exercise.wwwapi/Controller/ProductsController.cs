0e97179e37c46777b3a062ed12999d43a99f61de
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-26 12:59:13 +0100

    Changed to input whole objects instead of separate fields, also added error handling. a "new string("n")" as product price throws a 400 error with custom message!

--- exercise.wwwapi/Controller/ProductsController.cs
+++ exercise.wwwapi/Controller/ProductsController.cs
@@ -10,27 +10,48 @@ namespace exercise.wwwapi.Controller
         {
             var productsGroup = app.MapGroup("products");
 
-            productsGroup.MapPost("/", PostProduct);
             productsGroup.MapGet("/", GetProducts);
             productsGroup.MapGet("/{id}", GetSpecificProduct);
-            productsGroup.MapPut("/{id}", PutProduct);
+            productsGroup.MapPost("/", PostProduct).AddEndpointFilter(async (invocationContext, next) => 
+            {
+                var productPost = invocationContext.GetArgument<ProductPost>(1);
+                if ((productPost.Name == null) || (productPost.Category == null))
+                {
+                    return TypedResults.BadRequest("Name and/or category must be provided.");
+                } 
+                else if (productPost.Price < 0) 
+                {
+                    return TypedResults.BadRequest("Price must be a positive integer.");
+                }
+                else if (productPost.Price.GetType() != typeof(int)) 
+                {
+                    return TypedResults.BadRequest("Price must be an integer, something else was provided.");
+                }
+                return await next(invocationContext);
+            });
+            productsGroup.MapPut("/{id}", PutProduct).AddEndpointFilter(async (invocationContext, next) =>
+            {
+                var productId = invocationContext.GetArgument<int>(1);
+                var productPut = invocationContext.GetArgument<ProductPut>(2);
+                Console.WriteLine(productPut);
+                if (productPut.Price < 0)
+                {
+                    return TypedResults.BadRequest("Price must be a positive integer.");
+                }
+                else if (productPut.Price.GetType() != typeof(int))
+                {
+                    return TypedResults.BadRequest("Price must be an integer, something else was provided.");
+                }
+                return await next(invocationContext);
+            });
             productsGroup.MapDelete("/{id}", DeleteProduct);
         }
 
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public static async Task<IResult> PostProduct(IRepository repository, string name, string category, int price)
+        public static async Task<IResult> PostProduct(IRepository repository, ProductPost productPost)
         {
-            if ((name == null) || (category == null)) 
-            {
-                return TypedResults.BadRequest("Name and/or category must be provided.");
-            }
-
-            Product? prod = repository.PostProduct(new ProductPost(name, category, price));
-            if (price < 0) // Int will never not be int. That will never happen. Checking for positive int instead
-            {
-                return TypedResults.BadRequest("Price must be an integer, larger than 0, something else was provided.");
-            }
+            Product? prod = repository.PostProduct(productPost);
             if (prod == null) 
             {
                 return TypedResults.BadRequest("Product with the provided name already exists.");
@@ -49,7 +70,7 @@ namespace exercise.wwwapi.Controller
             }
             else 
             {
-                return TypedResults.NotFound("No products in the provided category were found");
+                return TypedResults.NotFound("No products in the provided category were found.");
             }
         }
 
@@ -71,10 +92,10 @@ namespace exercise.wwwapi.Controller
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public static async Task<IResult> PutProduct(IRepository repository, int id, string? name, string? category, int price) 
+        public static async Task<IResult> PutProduct(IRepository repository, int id, ProductPut productPut) 
         {
-            Tuple<Product?, int> res = repository.PutProduct(id, new ProductPut() { Name = name, Category = category, Price = price});
-            if (res.Item2 == 201)
+            Tuple<Product?, int> res = repository.PutProduct(id, productPut);
+            if (res.Item2 == 201 && res.Item1 != null)
             {
                 return TypedResults.Created($"/{res.Item1.Id}", res.Item1);
             } else if (res.Item2 == 400)
@@ -90,7 +111,7 @@ namespace exercise.wwwapi.Controller
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public static async Task<IResult> DeleteProduct(IRepository repository, int id) 
         {
-            Product prod = repository.DeleteProduct(id);
+            Product? prod = repository.DeleteProduct(id);
             if (prod is null)
             {
                 return TypedResults.NotFound("Product not found.");
--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,6 +1,7 @@
 using exercise.wwwapi.Controller;
 using exercise.wwwapi.Data;
 using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler(c => c.Run(async context =>
+{
+    var exception = context.Features
+        .Get<IExceptionHandlerPathFeature>()
+        .Error;
+    if (exception is BadHttpRequestException) 
+    {
+        var response = new { error = "Price must be a integer, something else was provided" };
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}));
+
 
 app.UseHttpsRedirection();
 
