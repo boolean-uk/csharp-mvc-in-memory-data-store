743f66b04250771521d5caaed335f2eb78ea676b
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-01-29 11:15:31 +0100

    Stian Andreas Ytterstad Nordvik C#3

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace exercise.wwwapi.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T Get(int id);
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(int id);
+        void Save();
+        
+    }
+}
