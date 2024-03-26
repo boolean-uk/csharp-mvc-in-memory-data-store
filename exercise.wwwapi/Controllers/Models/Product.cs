dc9c729d9457f5ba49fa46c468a61a341b70773a
Nikita-vB <n.vanberendonk@gmail.com>
2024-03-26 14:39:42 +0100

    Core completed

---
+++ exercise.wwwapi/Controllers/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace exercise.wwwapi.Controllers.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }   
+        public string category { get; set; }
+        public int price { get; set; }
+
+        public Product(int id, string name, string category, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.category = category;
+            this.price = price;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public String getCategory()
+        {
+            return this.category;
+        }
+
+        public int getPrice()
+        {
+            return this.price;
+        }
+    }
+}
---
+++ exercise.wwwapi/Controllers/Repository/ProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using exercise.wwwapi.Controllers.Models;
+using exercise.wwwapi.Controllers.Models.DTO;
+using exercise.wwwapi.Controllers.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Controllers.Repository
+{
+    public class ProductRepository<T> : IProductRepository<T> where T : class
+    {
+        private static int IdCounter = 1;
+        private static List<Product> _products = new List<Product>();
+        private DataContext _db;
+        //private DbSet<T> _products = null;
+
+        public ProductRepository(DataContext datacontext)
+        {
+            _db = datacontext;
+            //_products = _db.Set<T>();
+        }
+
+        public bool Add(Product product)
+        {
+            if (product != null)
+            {
+                int id = _products.Count == 0 ? 1 : _products.Max(x => x.id) + 1;
+                product.id = id;
+                _products.Add(product);
+                return true;
+            }
+            return false;
+        }
+
+        public void create(string name, string category, int price)
+        {
+            Product product = new Product(ProductRepository<T>.IdCounter++, name, category, price);
+            ProductRepository<T>._products.Add(product);
+        }
+
+        public void Delete(int id)
+        {
+            var item = find(id);
+            if (item.id == id)
+            {
+                _products.Remove(item);
+            }
+        }
+
+        public Product find(int id)
+        {
+            return ProductRepository<T>._products.First(product => product.getId() == id);
+        }
+
+        public List<Product> getAll()
+        {
+            return ProductRepository<T>._products;
+        }
+
+        public List<Product> getAll(string category)
+        {
+            return ProductRepository<T>._products.Where(x => x.category.ToLower() == category.ToLower()).ToList();
+        }
+
+        public Product Update(int id, ProductPut product)
+        {
+            if (ProductRepository<T>._products.Any(x => x.id == id))
+            {
+                Product productToUpdate = ProductRepository<T>._products.Where(x => x.id == id).First();
+                productToUpdate.name = product.name;
+                productToUpdate.category = product.category;
+                productToUpdate.price = product.price;
+                _db.SaveChanges();
+                return productToUpdate;
+            }
+            else
+            {
+
+                return null;
+            }
+        }
+    }
+}
