9ca194ccfb6eeed31b1008e4f93dea8d552e779d
Malte <malte.stengard@gmail.com>
2024-08-30 10:44:28 +0200

    Added collection for product

--- exercise.wwwapi/Controller/ProductController.cs
+++ exercise.wwwapi/Controller/ProductController.cs
@@ -1,6 +1,46 @@
-﻿namespace exercise.wwwapi.Controller
+﻿using exercise.wwwapi.Models.Data;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exercise.wwwapi.Controller
 {
+    [ApiController]
+    [Route("products")]
     public static class ProductController
     {
+        public static void ConfigureProductController(this WebApplication app)
+        {
+            var products = app.MapGroup("/");
+            products.MapGet("/", GetAll);
+            products.MapGet("/{id}", GetProduct);
+            products.MapPost("/", AddProduct);
+            products.MapPut("/", UpdateProduct);
+            products.MapDelete("/", DeleteProduct);
+        }
+
+        private static IResult DeleteProduct(IProductRepository repository, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IResult UpdateProduct(IProductRepository repository, int id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IResult AddProduct(IProductRepository repository, Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IResult GetProduct(IProductRepository repository, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IResult GetAll(IProductRepository repository)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
