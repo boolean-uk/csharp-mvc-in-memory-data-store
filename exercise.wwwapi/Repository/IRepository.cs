2d4c6451e4fc4e481130e7ca58bac185c62d321b
Ingeborg Brommeland Austeid <ingeborg_austeid@hotmail.com>
2024-08-30 14:52:07 +0200

    adds completed exercise with extensions

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using exercise.wwwapi.Models;
+using exercise.wwwapi.ViewModels;
+using System.Runtime.InteropServices;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IRepository
+    {
+        List<Product> GetAllProducts(string? category);
+
+        Product CreateProduct(Product product);
+
+        Product GetAProduct(int id);
+
+        Product UpdateProduct(int id, Product product);
+
+        Product DeleteProduct(int id);
+    }
+}
