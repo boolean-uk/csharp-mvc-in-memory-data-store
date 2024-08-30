c60a76463d0b08988f1822a6f0ccd24ac56f4556
Espen Luna <espensluna@gmail.com>
2024-08-30 15:48:52 +0200

    Core and extensions?

---
+++ exercise.wwwapi/Data/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using exercise.wwwapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Data
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ exercise.wwwapi/Endpoints/ProductEndpoints.cs
@@ -0,0 +1,81 @@
+﻿using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace exercise.wwwapi.Endpoints
+{
+    public static class ProductEndpoints
+    {
+
+        public static void ConfigureProductEndpoint(this WebApplication app)
+        {
+            var products = app.MapGroup("products");
+            products.MapGet("/getbycategory/{category?}", GetAllProducts);
+            products.MapGet("/getbyid/{id}", GetProduct);
+            products.MapPut("/{id}", UpdateProduct);
+            products.MapPost("/", AddProduct);
+            products.MapDelete("/{id}", DeleteProduct);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static IResult GetAllProducts(IRepository<Product> repo, string? category)
+        {
+            List<Product> products = repo.GetAllProducts(category);
+
+            if(products.Count == 0)
+            {
+                return TypedResults.NotFound(new ErrorModel());
+            }
+            return TypedResults.Ok(products);
+
+    
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static IResult GetProduct(IRepository<Product> repo, int id)
+        {
+            Product p = repo.GetProduct(id);
+            return p != null ? TypedResults.Ok(p) : TypedResults.NotFound(new ErrorModel());
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static IResult UpdateProduct(IRepository<Product> repo, int id, Product product)
+        {
+
+            if (repo.GetAllProducts(null).Any(p => p.Name == product.Name))
+            {
+                return TypedResults.BadRequest(new ErrorModel());
+            }
+            Product p = repo.UpdateProduct(id, product);
+
+            if (p == null)
+            {
+                return TypedResults.NotFound(new ErrorModel());
+            }
+            return TypedResults.Ok(p);
+            
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public static IResult AddProduct(IRepository<Product> repo, Product product)
+        {
+            Product p = repo.AddProduct(product);
+            return p != null ? TypedResults.Created("/", p) : TypedResults.BadRequest(new ErrorModel());
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static IResult DeleteProduct(IRepository<Product> repo, int id)
+        {
+            Product p = repo.DeleteProduct(id);
+            return p != null ? TypedResults.Ok(p) : TypedResults.NotFound(new ErrorModel());
+        }
+    }
+}
