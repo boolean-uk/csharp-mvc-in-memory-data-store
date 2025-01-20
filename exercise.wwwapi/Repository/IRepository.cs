1fddcb8c098010a0b53a2292f8f305870118afc6
GRehan <GRehan@N-NO-01-01-3380>
2025-01-20 15:21:20 +0100

    Finished Core & Extensions

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using exercise.wwwapi.Models;
+using exercise.wwwapi.ViewModel;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Product>> GetProducts(string category);
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(int id);
+        Task<Product> GetProductByName(string name);
+        Task<Product> DeleteProduct(int id);
+        Task<Product> AddProduct(Product product);
+        Task<Product> UpdateProduct(Product product, int id);
+    }
+}
