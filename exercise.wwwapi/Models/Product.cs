5b0d13cfa215fc05803ced3ee4e715a98044c04f
Hans Eivind Johnsen Skinstad <Hans.Eivind.Skinstad@no.experis.com>
2025-01-20 15:05:57 +0100

    Exercise finished with extensions

---
+++ exercise.wwwapi/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace exercise.wwwapi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; } = new Guid().GetHashCode();
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ exercise.wwwapi/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Repository
+{
+    public class Repository : IRepository
+    {
+        private DataContext _db;
+
+        public Repository(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Product> AddProduct(Product product)
+        {
+            await _db.Products.AddAsync(product);
+            await _db.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            _db.Products.Remove(await _db.Products.FindAsync(id));
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _db.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _db.Products.ToListAsync();        
+        }
+
+        public async Task<bool> UpdateProduct(int id)
+        {
+            var target = await _db.Products.FindAsync(id);
+            _db.Products.Update(target);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+    }
+}
