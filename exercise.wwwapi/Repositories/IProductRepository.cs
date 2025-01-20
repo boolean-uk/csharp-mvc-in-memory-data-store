8db14752119e61a2120a69bc251c8f44537cc1b5
Miadog7Extra <petter.karstensen@gmail.com>
2025-01-20 13:10:57 +0100

    Core & Extension

---
+++ exercise.wwwapi/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(int id);
+        Task<Product> UpdateProduct(Product product);
+        Task<bool> Delete(int id);
+        Task<Product> AddProduct(Product product);
+        Task<IEnumerable<Product>> GetProductsByCategory(string category);
+    }
+}
