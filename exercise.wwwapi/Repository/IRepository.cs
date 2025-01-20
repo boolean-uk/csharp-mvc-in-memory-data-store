20caee56442838e4bb19b6264de262b9d03ed8fd
Sander Rasmussen <sander.rasmussn@gmail.com>
2025-01-20 15:56:13 +0100

    fixed compilation error

--- exercise.wwwapi/Repository/IRepository.cs
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -5,7 +5,7 @@ namespace exercise.wwwapi.Repository
     public interface IRepository
     {
         Task<Product> Get(int id);
-        Task<IEnumerable<Product>> GetAll();
+        Task<IEnumerable<Product>> GetAll(string category = "");
         Task<IEnumerable<Product>> Delete(int id);
         Task<IEnumerable<Product>> Update(int id, string name, string category, int price);
         Task<IEnumerable<Product>> Add(Product entity);
