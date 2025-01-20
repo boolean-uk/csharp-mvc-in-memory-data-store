65b2460a4fab13e65a3a918c49722619f50456d3
magnus195 <22998711+magnus195@users.noreply.github.com>
2025-01-20 10:51:46 +0100

    Too early in the morning

--- exercise.wwwapi/Repositories/IRepository.cs
+++ exercise.wwwapi/Repositories/IRepository.cs
@@ -7,8 +7,8 @@ public interface IRepository<T> where T : class
     T? Read(Guid id);
     T Update(T entity);
     void Delete(Guid id);
-    
-    // Also part of "Create", just better
+
+    // Also part of "Read", just better
     T? GetByName(string name);
     List<T> GetAll();
     List<T> GetAll(string category);
