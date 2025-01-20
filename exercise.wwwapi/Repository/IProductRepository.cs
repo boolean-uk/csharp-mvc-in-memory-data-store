49fd72d4d8697a987bd01d95d3c28b8545ae34ee
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-20 16:33:27 +0100

    Core + Extension done

---
+++ exercise.wwwapi/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using exercise.wwwapi.Model;
+using exercise.wwwapi.View;
+
+namespace exercise.wwwapi.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts(string category);
+        Task<Product?> GetProduct(int id);
+        Task<Product?> GetProduct(string name);
+        Task<Product?> UpdateProduct(int id, Product_edit dto);
+        Task<Product?> DeleteProduct(int id);
+        Task<Product?> CreateProduct(Product_create dto);
+    }
+}
