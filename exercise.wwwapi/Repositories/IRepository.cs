a180d1515e97aba53cc115acf6121e6976678242
Andreas Conradi Nitschke <andreas.nitschke@experisacademy.no>
2025-01-20 10:16:16 +0100

    finished exercise

---
+++ exercise.wwwapi/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.Repositories
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Product>> GetProducts(string category);
+        Task<Product> GetProductById(int id);
+        Task<Product> AddProduct(Product product);
+        Task<Product> UpdateProduct(Product product, int id);
+        Task<Product> DeleteProduct(int id);
+    }
+}
---
+++ exercise.wwwapi/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Repositories
+{
+    public class ProductRepository : IRepository
+    {
+        private DataContext _db;
+        public ProductRepository(DataContext db)
+        {
+            _db = db;
+        }
+        public async Task<Product> AddProduct(Product product)
+        {
+            await _db.Products.AddAsync(product);
+            await _db.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> DeleteProduct(int id)
+        {
+            Product target = await _db.Products.FindAsync(id);
+            if (target != null)
+            {
+                _db.Products.Remove(target);
+                await _db.SaveChangesAsync();
+                return target;
+            }
+            return null;
+        }
+
+        public async Task<Product> GetProductById(int id)
+        {
+            return await _db.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts(string category)
+        {
+            return await _db.Products.Where(p => p.Category == category).ToListAsync();
+        }
+
+        public async Task<Product> UpdateProduct(Product product, int id)
+        {
+            Product target = await _db.Products.FindAsync(id);
+            if (target != null)
+            {
+                target.Price = product.Price;
+                target.Name = product.Name;
+                target.Category = product.Category;
+                _db.Update(target);
+                await _db.SaveChangesAsync();
+                return target;
+            }
+            return null;
+
+        }
+    }
+}
